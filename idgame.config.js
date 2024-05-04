d254332e68de1530171fc5704e509e18bf4399d9
Sterktez <73569420+Sterktez@users.noreply.github.com>
2024-05-05 02:47:15 +0700

    Add files via upload

---
+++ idgame.config.js
@@ -0,0 +1,16 @@
+// Cek ID Game Config
+
+let configIdGame = {
+   codashop: true, // enable this if you want to use codashop: CODM, Free Fire, and etc
+   duniagames: true, // enable this if you want to use duniagames: CODM, Free Fire, and etc
+   au2mobile: true, // enable this if you false to use au2mobile: AU2 Mobile
+   midasbuy: true, // enable this if you want to use midasbuy: PUBG, Tower of Fantasy, and etc
+   midasbuyPubg: true, // enable this if you want to use midasbuy: PUBG
+   midasbuyTOF: false, // enable this if you want to use midasbuy: Tower of Fantasy (NOT WORKING)
+   midasbuyNikkeGL: false, // enable this if you want to use midasbuy: Nikke GL (NOT WORKING)
+   midasbuyChimera: false, // enable this if you want to use midasbuy: Chimera (NOT WORKING)
+   midasbuyAlchemy: false, // enable this if you want to use midasbuy: Alchemy (NOT WORKING)
+   midasbuyPubgLite: false, // enable this if you want to use midasbuy: PUBG Lite (NOT WORKING)
+};
+
+module.exports = configIdGame;
